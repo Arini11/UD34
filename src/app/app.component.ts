af25431c4c58843e3f7da0bdb9f8db7e42f4cda4
Arini11 <elarnaumc@gmail.com>
2022-09-07 00:41:52 +0200

    ud34

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UD34';
+
+  txtInput = '0';
+  resultat = '0';
+
+  addNum(num: string) {
+    if(this.resultat != '0' && num != '00') {
+      this.txtInput = num;
+    }
+    else if(((this.txtInput == '0' || this.txtInput == '') || this.resultat != '0') && num == '00') {
+      this.txtInput = '0';
+    }
+    else if(this.txtInput == '0') {
+      this.txtInput = num;
+    }
+    else {
+      this.txtInput += num;
+    }
+
+    this.resultat = '0';
+  }
+
+  addOperator(op: string) {
+    if(this.resultat != '0') {
+      this.txtInput = this.resultat + op;
+    } else {
+      this.txtInput += op;
+    }
+
+    this.resultat = '0';
+  }
+
+  esborrar( ){
+    this.txtInput = this.txtInput.substring(0,this.txtInput.length-1);
+  }
+
+  calcular() {
+    this.resultat = eval(this.txtInput);
+  }
 }
